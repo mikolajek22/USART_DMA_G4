8048803186e173041efc5d7469b941db481b02d3
Mikolaj Pietrynko <mikopietrynko@gmail.com>
2025-02-19 19:41:30 +0100

    small fix

--- Core/Src/stm32g4xx_it.c
+++ Core/Src/stm32g4xx_it.c
@@ -20,6 +20,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32g4xx_it.h"
+#include "usart_dma.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 /* USER CODE END Includes */
--- Debug/Core/Src/crc8.d
+++
@@ -1,2 +0,0 @@
-Core/Src/crc8.o: ../Core/Src/crc8.c ../Core/Inc/crc8.h
-../Core/Inc/crc8.h:
